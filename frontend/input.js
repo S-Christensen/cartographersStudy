00e0f9cfb3dff39342067c258c308a896720712f
MonsieurMantis <official2904@gmail.com>
2025-11-10 19:49:43 -0500

    reassigned variable like a dum dum

--- frontend/input.js
+++ frontend/input.js
@@ -69,7 +69,7 @@ window.addEventListener("DOMContentLoaded", () => {
   }
   const card = localStorage.getItem("currentCard");
   if (card) {
-    let currentCard = JSON.parse(card);
+    Object.assign(currentCard, JSON.parse(card));
     console.log("Loaded card from localStorage:", currentCard);
     if (currentCard.flag) {
       alert("You have a ruins card to place!");
