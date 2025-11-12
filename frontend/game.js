e11fc47b908d0c1c66a71e95ae36d2a4306875e0
MonsieurMantis <official2904@gmail.com>
2025-11-12 10:13:54 -0500

    sanitize payload

--- frontend/game.js
+++ frontend/game.js
@@ -130,10 +130,9 @@ export async function submitMove() {
   fetchSession();
   const playerToken = localStorage.getItem("playerToken"); // secure backend-issued token
   
-  const sanitizedCard = {
-    ...currentCard,
-    shape: currentCard.shape.map(row => row.map(cell => parseInt(cell, 10)))
-  };
+  const sanitizedCard = currentCard
+  sanitizedCard.shape = currentCard.shape.map(row => row.map(cell => String(cell)));
+  
   console.log("Current Card on submit:", sanitizedCard);
   const payload = {
     new_grid: gridData.map(row => row.map(cell => String(cell))),
