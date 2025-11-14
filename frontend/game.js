5bb96ceb1e09d9a867f8560a129af7d7659a5507
MonsieurMantis <official2904@gmail.com>
2025-11-13 21:51:52 -0500

    forgot import

--- frontend/game.js
+++ frontend/game.js
@@ -1,4 +1,4 @@
-import { getColor, highlightCurrentSeason, renderScoringCards, renderShapePreview, showShapeButtons, showTerrainButtons } from './ui.js';
+import { getColor, highlightCurrentSeason, renderScoringCards, renderShapePreview, showShapeButtons, showTerrainButtons, updateCoinTracker } from './ui.js';
 
 export const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
